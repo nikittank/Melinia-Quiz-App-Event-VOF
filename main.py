54d2d8aa6b412e7a0e5be280566f88332e4e6cf8
Nikitta <91609970+nikittank@users.noreply.github.com>
2025-03-07 10:33:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -202,7 +202,7 @@ else:
 
 
 # Initialize Groq client
-    client = Groq(api_key="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")
+    client = Groq(api_key="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")
 # Define quiz questions in a dictionary
     questions = [
     {"question": "To unlock this, use the four letters below and create a seven-letter word: 'UMNI'. \n\nWin Amount : $1000", "answer": [77, 73, 78, 73, 77, 85, 77],"amount":1000},
